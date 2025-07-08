7f11f163545b92875d265fe480b41f0159bbadeb
ChilliSauce <navshukla30@gmail.com>
2025-07-08 18:15:18 +0530

    Basic Sructure

---
+++ app/src/db/client.js
@@ -0,0 +1,12 @@
+import pg from "pg";
+import "dotenv/config";
+
+const db = new pg.Pool({
+  user: process.env.PGUSER,
+  host: process.env.PGHOST,
+  database: process.env.PGDATABASE,
+  password: process.env.PGPASSWORD,
+  port: process.env.PGPORT,
+});
+
+export default db;
--- app/src/index.js
+++ app/src/index.js
@@ -1,7 +1,11 @@
 import express from "express";
+import cors from "cors";
 
 const app = express();
 const port = 3000;
+
+app.use(cors());
+app.use(express.json());
 
 app.listen(port, () => {
   console.log(`Server running on port ${port}`);
