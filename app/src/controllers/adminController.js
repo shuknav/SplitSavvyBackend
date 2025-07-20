4ad07d7a6d940573d1aa1cfac2b8b9eb9aa4c114
ChilliSauce <navshukla30@gmail.com>
2025-07-20 22:58:28 +0530

    feat: admin password update validation using bcrypt

--- app/src/controllers/adminController.js
+++ app/src/controllers/adminController.js
@@ -3,6 +3,8 @@ import bcrypt from "bcrypt";
 import "dotenv/config";
 import jwt from "jsonwebtoken";
 
+const saltRounds = 12;
+
 export const AdminIdentiyVerify = async (req, res) => {
   const { username, password } = req.body;
   const response = await db.query(
@@ -24,6 +26,41 @@ export const AdminIdentiyVerify = async (req, res) => {
         res.json({ result: true, message: "welcome", token });
       } else {
         res.json({ result: false, message: "wrngpass" });
+      }
+    }
+  });
+};
+
+export const PasswordUpdate = async (req, res) => {
+  const { oldPassword, newPassword } = req.body;
+  const username = "whisker";
+  const response = await db.query(
+    "SELECT * FROM admins where username = ($1)",
+    [username]
+  );
+  const storedPassword = response.rows[0].hashed_password;
+  bcrypt.compare(oldPassword, storedPassword, (err, result) => {
+    if (err) {
+      console.log(err);
+    } else {
+      if (result) {
+        bcrypt.hash(newPassword, saltRounds, async (error, hash) => {
+          if (error) {
+            console.log(error);
+          } else {
+            try {
+              const response = await db.query(
+                "UPDATE admins SET hashed_password = ($1) WHERE username = ($2)",
+                [hash, username]
+              );
+              res.json({ result: "Success" });
+            } catch (err) {
+              console.log(err);
+            }
+          }
+        });
+      } else {
+        res.json({ result: "incorrect current password" });
       }
     }
   });
--- app/src/routes/adminRoutes.js
+++ app/src/routes/adminRoutes.js
@@ -2,6 +2,7 @@ import express from "express";
 import {
   AdminIdentiyVerify,
   TokenVerify,
+  PasswordUpdate,
 } from "../controllers/adminController.js";
 
 const router = express.Router();
@@ -9,5 +10,6 @@ const router = express.Router();
 router.post("/verify", AdminIdentiyVerify); // route to handle admin verification
 // router.post("/add", AdminDetailAdd); // route to add new admin details
 router.get("/tokenverify", TokenVerify);
+router.post("/passwordupdate", PasswordUpdate);
 
 export default router;
